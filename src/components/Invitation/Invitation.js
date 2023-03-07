d99df51c01b5ab440d5a3b466ed76ec8a20138e2
Joanna Makarewicz <joanna.milun@gmail.com>
2023-03-07 09:57:50 +0100

    Invitation section

--- src/components/Invitation/Invitation.js
+++ src/components/Invitation/Invitation.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
+import { DiReact } from "react-icons/di";
+import "./Invitation.scss";
 
 const Invitation = () => {
   return (
-    <div>
-      
+    <div className="container w-7">
+      <h1 className="mb-5 text-center">
+        Welcome on my website build for practice{" "}
+        <span className="invitation__special">
+          <DiReact />
+          React
+        </span>{" "}
+        skills 💪!
+      </h1>
+      <div className="container d-flex text-start w-75 lh-lg ">
+        <p>
+          My name is Joanna and I’m an aspiring Frontend developer. Currently
+          I’m looking for a new challenges so if you have one -
+          <a href="/" class="invitation__special2">
+            contact me
+          </a> 
+           😊
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Invitation;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Invitation from '../../components/Invitation';
 import Navbar from '../../components/Navbar';
+import "./Home.scss";
 
 const Home = () => {
   return (
+    <div className='home pb-4'>
+      <div >
       <Navbar/>
+      <Invitation/>
+      </div>
+
+    </div>
+
   )
 }
 
