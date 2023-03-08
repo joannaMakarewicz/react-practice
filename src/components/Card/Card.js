1f78d5577f194ec74fd2a5ec7bd5b1b233b86c67
Joanna Makarewicz <joanna.milun@gmail.com>
2023-03-08 14:50:59 +0100

    linear-gradient in buttons added

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React from "react";
 import "./Card.scss";
-import img from "../../assets/img/clock.png";
+import img from "../../assets/img/hotels.png";
+
+
 
 const Card = (props) => {
+
   return (
-            <div className="card">
-              <img src={img} class="card-img-top" alt="/"/>
-              <div className="card-body p-3">
-                <h3 className="card-title text-center">{props.project.name}</h3>
-                <p className="card-text">{props.project.description}</p>
-                <a href={props.project.homepage} class="card__button btn" target="_blank" rel="noreferrer">demo</a>
-                <a href={props.project.html_url} class=" card__button2 btn ms-3" target="_blank" rel="noreferrer">github</a>
-              </div>
-            </div>
-  )
-}
+    <div className="card">
+      <img src={img} className="card-img-top" alt="/" />
+      <div className="card-body p-3">
+        <h3 className="card-title text-center">{props.project.name}</h3>
+        <p className="card-text">{props.project.description}</p>
+        <div className="d-flex justify-content-end">
+
+                <a
+                    href={props.project.homepage}
+                    class="card__button btn"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    demo
+                </a>
+                <a
+                    href={props.project.html_url}
+                    class="card__button btn ms-3"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    github
+                </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
